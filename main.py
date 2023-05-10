d869784860c2e8ed0bb620c337c0af6c2f329177
aakimovkgz <arturakimov1402@icloud.com>
2023-05-10 19:19:48 +0600

    Service connected

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-
+from service import transfer
 
 def main():
-    pass
+    while True:
+        user_action = input('Enter 1 to transfer\nEnter 2 to exit\n')
+        if user_action == '1':
+            transfer()
+        elif user_action == '2':
+            print('Goodbye!')
+            break
+        else:
+            print('Inccorect command!')
 
 if __name__ == '__main__':
     main()
